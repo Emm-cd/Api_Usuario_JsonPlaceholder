13747215d07e8c71e5a46838c99acb9f36cc57c8
Emma-cd <emmarj1909@gmail.com>
2024-10-12 18:36:23 -0600

    first commit

---
+++ src/app/universidades/[id]/page.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+async function getUni() {
+    const url="http://universities.hipolabs.com/search?country=Mexico"
+    const universi = await axios.get(url);
+    //console.log(universidades.data);
+    return universi.data;
+}
+
+export default async function Universidad({params}){
+    const universidad = await getUni();
+    return(
+        <>
+        <h1>Estas en universidades</h1>
+        <p>{params.id}</p>
+        </>
+    );
+}
